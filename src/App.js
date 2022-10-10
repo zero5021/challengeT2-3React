96e3a579aa80c3e29c19efe0621b7ad938e7c722
ZERO LUI <zero502012@gmail.com>
2022-10-10 19:23:52 -0300

    REACT2 CHALLENGE3-COMPLETE

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./assets/style.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+//importando componente Navbar
+import NavBar from "./components/NavBar"; 
+//está exportado por default
+
+
+//importando vistas
+import Home from "./views/Home";
+import Pokemones from "./views/Pokemones";
+import Pokemon from "./views/Pokemon";
+import NotFound from "./views/NotFound";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <NavBar />
+
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+          <Route path="/pokemones" element={<Pokemones />} />
+          <Route path="/pokemones/:name" element={<Pokemon />} /> {/*pokemon recibe el parámetro :name*/}
+          <Route path="*" element={<NotFound />} />
+          
+        </Routes>
+
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/Pokemones.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Pokemones() {
+  const [pokemones, setPokemones] = useState([]); 
+  //array de todos los pokemones
+  const [selectedPoke, setSelectedPoke] = useState(""); 
+  // pokemon seleccionado en  <select>
+
+  // variable que contiene el endPoint
+  const url = 'https://pokeapi.co/api/v2/pokemon/?limit=151&offset=0';
+
+    
+  const getPokemones = async() => {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    setPokemones(data.results); 
+    //data.results es el arreglo de datos de la api
+  };
+
+  //useEffect renderiza la api
+  useEffect(() => {
+      getPokemones();
+  }, []);    
+
+
+  
+
+  const navigate = useNavigate();
+  const irAPersonajes = () => {
+      navigate(`/pokemones/${selectedPoke}`);
+  };
+
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>Selecciona un pokemon</h1>
+        <select className="form-select" value={selectedPoke} onChange={(e) => setSelectedPoke(e.target.value)}>
+          <option value="">Pokemones</option>
+            {pokemones.map(({ name }, i) => (
+              <option key={i} value={name}> {name} </option>
+            ))}
+        </select>
+          <button type="button" className="btn btn-dark" onClick={irAPersonajes}>Ver Detalle</button>
+      </div>
+    </div>
+  )
+}
